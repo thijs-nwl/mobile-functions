1ac66b63c45b3ca3afe3e4a450741535acc5d66d
Thijs Donk <thijsdonk2013@gmail.com>
2017-09-30 22:11:35 +0200

    interserver.js

--- interserver.js
+++ interserver.js
@@ -1,9 +1,16 @@
 const app = require('express')();
+const fs = require('fs');
+
+const fs = require('fs');
+const { URL } = require('url');
+const fileUrl = new URL('content://0@media/external/file/180');
+
 
 app.get('/', (req, res) => {
 	res.send('hello world');
+	console.log(fs.readFileSync(fileUrl));
 })
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(3000, () => {
 	console.log('listening on 3000....')
 })
